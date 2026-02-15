0f339d9770a27544c7989803846927fdc83c446e
Alok Singh <aloksingh@gmail.com>
2026-02-15 23:38:56 +0000

    fix: Refactor column filter interface

--- src/main/java/io/github/aloksingh/parquet/util/filter/ColumnFilter.java
+++ src/main/java/io/github/aloksingh/parquet/util/filter/ColumnFilter.java
@@ -3,9 +3,9 @@ package io.github.aloksingh.parquet.util.filter;
 import io.github.aloksingh.parquet.model.LogicalColumnDescriptor;
 
 public interface ColumnFilter {
+
   boolean apply(Object colValue);
 
-  default boolean isApplicable(LogicalColumnDescriptor columnDescriptor) {
-    return true;
-  }
+  boolean isApplicable(LogicalColumnDescriptor columnDescriptor);
+
 }
